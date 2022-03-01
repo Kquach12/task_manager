9b01e5db1448e2f0ad3acee137d6b6ff134c6ec1
Kenny Quach <kennyquach60@gmail.com>
2022-02-28 19:24:46 -0800

    Mark as complete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import './styles.css'
 import {Todo} from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import {MdDone} from 'react-icons/md'
+import TodoList from './TodoList'
 
 
 interface Props {
@@ -12,14 +13,27 @@ interface Props {
 }
 
 const SingleTodo = ({todo, todoArray, setTodoArray}:Props) => {
+
+    const handleDone = (id:number) =>{
+        setTodoArray(todoArray.map((todo)=>
+            todo.id == id ? {...todo, isDone: !todo.isDone} : todo))
+    }
+
     return(
         <div >
             <form className="todos_single">
-                <span className='todos_single-text'>{todo.todo}</span>
+                {
+                    todo.isDone?(
+                        <s className='todos_single-text'>{todo.todo}</s>
+                        )
+                        : (
+                        <span className='todos_single-text'>{todo.todo}</span>
+                        )
+                }
                 <div>
                     <span className="icon"><AiFillEdit/></span>
                     <span className="icon"><AiFillDelete/></span>
-                    <span className="icon"><MdDone/></span>
+                    <span className="icon" onClick={()=>handleDone(todo.id)}><MdDone/></span>
                 </div>
             </form>
         </div>
